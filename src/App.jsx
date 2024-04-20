5510f494f8c5dfaa5eb31ac4a2edc24dad85ab81
ZarryMyles <mahanth2709@gmail.com>
2024-04-20 19:22:12 +0530

    Init Vite app with Tailwind
    - Cleaned started code and files
    - Set up prettier config
    - Installed and set up tailwind

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+	return (
+		<div className='bg-slate-700 text-white w-screen h-screen flex items-center justify-center capitalize'>
+			helluva world
+		</div>
+	)
+}
+
+export default App
